981e3823fd8196ae5e670d1b39fb6f5448ff4936
seen-idc <seen-idc@github.com>
2021-11-06 18:27:55 +1100

    added bundle command and rank history command

--- api.py
+++ api.py
@@ -2,17 +2,35 @@ import requests
 import json
 
 def get_profile(name, tag):
-    x = requests.get('https://api.henrikdev.xyz/valorant/v1/account/{}/{}'.format(name, tag))
+    req = requests.get('https://api.henrikdev.xyz/valorant/v1/account/{}/{}'.format(name, tag))
     
-    if (not x.status_code == 200):
+    if (not req.status_code == 200):
         return False
     else:
-        return x.json()
+        return req.json()
 
 def get_mmr(region, name, tag):
-    x = requests.get('https://api.henrikdev.xyz/valorant/v2/mmr/{}/{}/{}'.format(region, name, tag))
+    req = requests.get('https://api.henrikdev.xyz/valorant/v2/mmr/{}/{}/{}'.format(region, name, tag))
     
-    if (not x.status_code == 200):
+    if (not req.status_code == 200):
         return False
     else:
-        return x.json()
+        return req.json()
+
+def get_store_bundle():
+    req = requests.get('https://api.henrikdev.xyz/valorant/v1/store-featured')
+    
+    if (not req.status_code == 200):
+        return False
+    else:
+        data = req.json()['data']
+        featured = data['FeaturedBundle']
+        bundle = featured['Bundle']
+        asset_id = bundle['DataAssetID']
+
+        bundle_req = requests.get('https://valorant-api.com/v1/bundles/{}'.format(asset_id))
+
+        if (not bundle_req.status_code == 200):
+            return False
+
+        return bundle_req.json()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from nextcord.ext import commands
 from yaml import load as load_yaml, Loader
 from math import floor
 from re import search
+from random import randint
 import api
 
 def load_config():
@@ -14,11 +15,21 @@ def load_config():
 def add_empty_field(embed):
     embed.add_field(name='\u200b', value='\u200b', inline=True)
 
+def add_act(act_name, data, embed: Embed):
+    embed.add_field(name=act_name, value='```yml\nRank: {}\nWins: {}\nGames: {}\nWinrate: {:.1f}%```'.format(data['final_rank_patched'], data['wins'], data['number_of_games'], data['wins'] / data['number_of_games'] * 100))
+
 def positive_or_negative(num):
     if (num < 0):
         return '-'
     else:
         return '+'
+
+def random_display_for_bundle():
+    random = randint(1,2)
+    if (random == 1):
+        return 'displayIcon'
+    else:
+        return 'displayIcon2'
 
 valorant_name_regex = '^.+#.+$'
 
@@ -31,7 +42,59 @@ bot = commands.Bot(command_prefix=config['prefix'])
 async def ping(ctx):
     await ctx.reply('Pong! `{}ms`'.format(floor(bot.latency * 1000)))
 
-@commands.cooldown(1, 8, commands.BucketType.user)
+@commands.cooldown(1, 15, commands.BucketType.user)
+@bot.command(name='store')
+async def val_store(ctx):
+    bundle = api.get_store_bundle()
+    
+    if (not bundle):
+        return await ctx.reply('An error has occurred.')
+    
+    data = bundle['data']
+    
+    bundle_embed = Embed(title='**{}**'.format(data['displayName']), description='**{}**\n{}'.format(data['description'], data['extraDescription']), color=0x001b3b)
+    bundle_embed.set_image(url=data[random_display_for_bundle()])
+
+    await ctx.send(embed=bundle_embed)
+
+@commands.cooldown(1, 15, commands.BucketType.user)
+@bot.command(name='rank-history')
+async def mmr(ctx, *args):
+    profile_text = ' '.join(args)
+    await ctx.reply('Fetching Rank history for `{}` ...'.format(profile_text))
+
+    if (not search(valorant_name_regex, profile_text)):
+        return await ctx.reply('The valorant profile must be in the format `name#tag`')
+
+    name_tag = profile_text.split('#')
+
+    profile = api.get_profile(name_tag[0], name_tag[1])
+    
+    if (not profile):
+        return await ctx.reply('An error has occurred.')
+    
+    profile_data = profile['data']
+    profile_card = profile_data['card']
+
+    mmr = api.get_mmr(profile_data['region'], name_tag[0], name_tag[1])
+
+    if (not mmr):
+        return await ctx.reply('An error has occurred.')
+    
+    mmr_data = mmr['data']
+    seasons = mmr_data['by_season']
+
+    profile_embed = Embed(title='Rank history for: **{}#{}**'.format(name_tag[0], name_tag[1]), color=0xF24D4E)
+    
+    profile_embed.set_author(name='{}#{}'.format(profile_data['name'], profile_data['tag']), icon_url=profile_card['small'])
+
+    for key in seasons:
+        add_act(key.upper(), seasons[key], profile_embed)
+    
+    await ctx.send(embed=profile_embed)
+
+
+@commands.cooldown(1, 20, commands.BucketType.user)
 @bot.command()
 async def profile(ctx, *args):
     profile_text = ' '.join(args)
@@ -63,11 +126,20 @@ async def profile(ctx, *args):
     current_rating = current_data['ranking_in_tier']
     current_rank = current_data['currenttierpatched']
     last_game_rating_change = current_data['mmr_change_to_last_game']
+    seasons = mmr_data['by_season']
 
     add_empty_field(profile_embed)
     profile_embed.add_field(name='Current Rank', value='**```yml\n{}```**'.format(current_rank), inline=True)
     profile_embed.add_field(name='Current RR', value='```yml\n{}```'.format(current_rating), inline=True)
     profile_embed.add_field(name='Latest Game', value='```diff\n{} {}```'.format(positive_or_negative(last_game_rating_change), last_game_rating_change), inline=True)   
+
+    i = 0
+    for key in seasons:
+        i += 1
+        add_act(key.upper(), seasons[key], profile_embed)
+        if (i == 2):
+            break
+
     await ctx.send(embed=profile_embed)
 
 
